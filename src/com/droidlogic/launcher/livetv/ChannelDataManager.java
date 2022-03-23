ea437e62c821945cb1925480a9ead65cb6349713
YeFeiWang <yefei.wang@amlogic.com>
2022-03-23 11:01:37 +0800

    TvLauncher: DTV does not display no channel [1/1]
    PD#000000
    
    Problem:
    if no current channle,it will get first channel from current input source.
    atv and dtv has the same input id,dtv get a error channel
    
    Solution:
    get the correct first channel from input source
    
    Verify:
    T3
    
    Change-Id: I6c3e874380556ceb52efe3c181142eccf0b8112a
    Signed-off-by: YeFeiWang <yefei.wang@amlogic.com>

--- src/com/droidlogic/launcher/livetv/ChannelDataManager.java
+++ src/com/droidlogic/launcher/livetv/ChannelDataManager.java
@@ -201,6 +201,7 @@ public class ChannelDataManager {
                         break;
                     }
                 }
+                channel = null;
             }
         }
         return channel;
